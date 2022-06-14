09d19334ebacafd5f8c6f3366ef21ff70fde84a4
2K21/CSE/15 Ali Murtaza <107362944+Alimurtaza2k21CSE15@users.noreply.github.com>
2022-06-15 00:43:38 +0500

    My Method program
    My name is Ali Murtaza ,I am Belong to Pakistan Sindh Nawab Shah And Currently i am studying at university of sindh jamshoro in Computer Science Department .

---
+++ method.java
@@ -0,0 +1,30 @@
+
+public class method {
+	static void method(Comparable<String> string) {
+	
+		System.out.println("2K21/CSE/15 Ali Murtaza");
+		
+		System.out.println("Assignment Of Software Engineering");
+	
+	}
+		public static void main(String[]org) {
+			
+			method("Roll No");
+			
+			method("2K21/CSE/15");
+			
+			method("Name");
+			
+			method("Ali Murtaza");
+			
+			method("Surname");
+			
+			method("Detho");
+			
+			method("Project Submitted To");
+			
+			method("Muhattaram JanaaB Sir Gulsher Laghari Sahab");
+	
+
+}
+}
